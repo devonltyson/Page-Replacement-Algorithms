1898efe2aecb3f60459c79cf77459ea8d0e8934b
Devon Tyson <devonltyson@txstate.edu>
2019-11-24 16:02:52 -0600

    updated main

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,29 @@ public class Main {
 
     private static final int SIZE = 100;
     private static final int UPPER_BOUND = 50;
+    private static int[] refString;
 
     public static void main(String[] args) {
 
-        int[] refString = createReferenceString(); // make reference String for page numbers
+        refString = createReferenceString(); // make reference String for page numbers
         int pageFrameCount = Integer.parseInt(args[0]); // get page frame count from command line arg
 
-        // create replacement algorithms
+        // create replacement algorithms and insert reference strings
         ReplacementAlgorithm fifo = new FIFO(pageFrameCount);
-        ReplacementAlgorithm lru = new LRU(pageFrameCount);
-        ReplacementAlgorithm opt = new OPT(pageFrameCount);
+        insertRefString(fifo);
 
+        ReplacementAlgorithm lru = new LRU(pageFrameCount);
+        insertRefString(lru);
+
+        ReplacementAlgorithm opt = new OPT(pageFrameCount);
+        insertRefString(opt);
+    }
+
+    // insert refString into algorithms
+    public static void insertRefString(ReplacementAlgorithm alg) {
+        for (int i = 0; i < SIZE; i++) {
+            alg.insert(refString[i]);
+        }
     }
 
     // create a reference String
